c7c7da6e755a9d28a56d91a566c600773d0dde4d
Kim Burgess <kim@acaprojects.com>
2020-02-03 15:53:07 +1000

    fix: preserve request ID if already set upstream

--- src/controllers/application.cr
+++ src/controllers/application.cr
@@ -3,15 +3,21 @@ require "uuid"
 abstract class Application < ActionController::Base
   before_action :set_request_id
   before_action :set_date_header
+  before_action :log_client_ip
   before_action :authorize!
 
   def set_request_id
-    logger.client_ip = client_ip
-    response.headers["X-Request-ID"] = logger.request_id = UUID.random.to_s
+    request_id = request.headers["X-Request-ID"]? || UUID.random.to_s
+    response.headers["X-Request-ID"] = request_id
+    logger.request_id = request_id
   end
 
   def set_date_header
     response.headers["Date"] = HTTP.format_time(Time.utc)
+  end
+
+  def log_client_ip
+    logger.client_ip = client_ip
   end
 
   def authorize!
