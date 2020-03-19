442a6d1f53bfb37ba86455248bd1e72c06041062
Kim Burgess <kim@acaprojects.com>
2020-03-19 22:29:43 +1000

    fix: wrap controller in PlaceOS::Analytics module

--- src/controllers/application.cr
+++ src/controllers/application.cr
@@ -1,26 +1,28 @@
 require "uuid"
 
-abstract class Application < ActionController::Base
-  before_action :set_request_id
-  before_action :set_date_header
-  before_action :log_client_ip
-  before_action :authorize!
+module PlaceOS::Analytics
+  private abstract class Application < ActionController::Base
+    before_action :set_request_id
+    before_action :set_date_header
+    before_action :log_client_ip
+    before_action :authorize!
 
-  def set_request_id
-    request_id = request.headers["X-Request-ID"]? || UUID.random.to_s
-    response.headers["X-Request-ID"] = request_id
-    logger.request_id = request_id
-  end
+    def set_request_id
+      request_id = request.headers["X-Request-ID"]? || UUID.random.to_s
+      response.headers["X-Request-ID"] = request_id
+      logger.request_id = request_id
+    end
 
-  def set_date_header
-    response.headers["Date"] = HTTP.format_time(Time.utc)
-  end
+    def set_date_header
+      response.headers["Date"] = HTTP.format_time(Time.utc)
+    end
 
-  def log_client_ip
-    logger.client_ip = client_ip
-  end
+    def log_client_ip
+      logger.client_ip = client_ip
+    end
 
-  def authorize!
-    head :forbidden unless request.headers["X-API-Key"]? == API_KEY
+    def authorize!
+      head :forbidden unless request.headers["X-API-Key"]? == App::API_KEY
+    end
   end
 end
--- src/controllers/ingest.cr
+++ src/controllers/ingest.cr
@@ -1,51 +1,53 @@
 require "flux"
 require "../models/event"
 
-class Ingest < Application
-  base "/ingest"
+module PlaceOS::Analytics
+  class Ingest < Application
+    base "/ingest"
 
-  # Accepts PlaceOS Analytics event stream objects as JSON lines within the
-  # event body.
-  post "/", :batch_ingest do
-    body = request.body
-    if body
-      body.each_line &->ingest(String)
-      head :accepted
-    else
-      head :bad_request
+    # Accepts PlaceOS Analytics event stream objects as JSON lines within the
+    # event body.
+    post "/", :batch_ingest do
+      body = request.body
+      if body
+        body.each_line &->ingest(String)
+        head :accepted
+      else
+        head :bad_request
+      end
     end
-  end
 
-  # Accepts a stream of PlaceOS Analytics events. Each message should contain a
-  # singular event for storage.
-  ws "/", :stream_ingest do |socket|
-    socket.on_message &->ingest(String)
-  end
+    # Accepts a stream of PlaceOS Analytics events. Each message should contain a
+    # singular event for storage.
+    ws "/", :stream_ingest do |socket|
+      socket.on_message &->ingest(String)
+    end
 
-  # Parses a received event and stores it.
-  private def ingest(event : String) : Nil
-    store PlaceOS::Analytics::Event.from_json event
-  rescue ex : JSON::ParseException
-    logger.warn { "invalid event format: #{event}"}
-  end
+    # Parses a received event and stores it.
+    private def ingest(event : String) : Nil
+      store PlaceOS::Analytics::Event.from_json event
+    rescue ex : JSON::ParseException
+      logger.warn { "invalid event format: #{event}"}
+    end
 
-  # Saves an event.
-  private def store(event : PlaceOS::Analytics::Event)
-    # InfluxDB stores points based on a unique keying of time, measure and tag
-    # values. If multiple points share these they will be overwritten. This
-    # occurs when events are sourced from an input with course time granularity
-    # (e.g. a bulk import of manually entered data - think observations at
-    # 9:30am). To enable pivoting to restore original associations for all
-    # fields in a point, introduce a 20 bits of noise (< 1.05ms) to the
-    # timestamp as a hash of the location identifier.
-    loc_hash = event.location.hash
-    loc_hash >>= 4
-    noise = (loc_hash ^ (loc_hash >> 20) ^ (loc_hash >> 40)) & 0xfffff
-    ts = event.time + noise.nanoseconds
+    # Saves an event.
+    private def store(event : PlaceOS::Analytics::Event)
+      # InfluxDB stores points based on a unique keying of time, measure and tag
+      # values. If multiple points share these they will be overwritten. This
+      # occurs when events are sourced from an input with course time granularity
+      # (e.g. a bulk import of manually entered data - think observations at
+      # 9:30am). To enable pivoting to restore original associations for all
+      # fields in a point, introduce a 20 bits of noise (< 1.05ms) to the
+      # timestamp as a hash of the location identifier.
+      loc_hash = event.location.hash
+      loc_hash >>= 4
+      noise = (loc_hash ^ (loc_hash >> 20) ^ (loc_hash >> 40)) & 0xfffff
+      ts = event.time + noise.nanoseconds
 
-    point = Flux::Point.new! event.type, ts, **event.fields
-    point.tag **event.tags
+      point = Flux::Point.new! event.type, ts, **event.fields
+      point.tag **event.tags
 
-    Flux.write point
+      Flux.write point
+    end
   end
 end
